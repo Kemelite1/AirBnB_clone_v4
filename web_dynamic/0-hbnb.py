949fa6f2823440f06319c90f4f17f16435a02dbf
kumdan job <redeks123456@gmail.com>
2023-11-03 13:12:35 +0100

    m

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
-from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
 from os import environ
+
 from flask import Flask, render_template
+
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.state import State
 
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
@@ -36,7 +38,7 @@ def hbnb():
     places = sorted(places, key=lambda k: k.name)
 
     return render_template(
-        "100-hbnb.html", states=st_ct, amenities=amenities, places=places
+        "0-hbnb.html", states=st_ct, amenities=amenities, places=places
     )
 
 
